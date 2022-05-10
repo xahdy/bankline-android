b32604cf083d0e183643aa151860332d92b4dee0
Marlon <marlon.schemberger@gmail.com>
2022-05-10 20:40:33 -0300

    Exibindo lista de movimentações com dados de exemplo.

--- app/src/main/java/com/dio/bankline/android/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/com/dio/bankline/android/ui/statement/BankStatementActivity.kt
@@ -3,10 +3,13 @@ package com.dio.bankline.android.ui.statement
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dio.bankline.android.R
 import com.dio.bankline.android.databinding.ActivityBankStatementBinding
 import com.dio.bankline.android.databinding.ActivityWelcomeBinding
 import com.dio.bankline.android.domain.Correntista
+import com.dio.bankline.android.domain.Movimentacao
+import com.dio.bankline.android.domain.TipoMovimentacao
 
 class BankStatementActivity : AppCompatActivity() {
 
@@ -23,6 +26,19 @@ class BankStatementActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        Log.d("teste", "Chegou o ID: ${accountHolder.id}")
+        binding.rvBankStatement.layoutManager = LinearLayoutManager(this)
+
+        findBankStatement()
     }
+
+    private fun findBankStatement() {
+        val dataSet = ArrayList<Movimentacao>()
+        dataSet.add(Movimentacao(1, "03/05/2022 09:24:55", "Lorem Ipsum", 1000.0, TipoMovimentacao.RECEITA,1))
+        dataSet.add(Movimentacao(1, "03/05/2022 09:24:55", "Lorem Ipsum", 4000.0, TipoMovimentacao.RECEITA,1))
+        dataSet.add(Movimentacao(1, "03/05/2022 09:24:55", "Lorem Ipsum", 3000.0, TipoMovimentacao.DESPESA,1))
+        dataSet.add(Movimentacao(1, "03/05/2022 09:24:55", "Lorem Ipsum", 2000.0, TipoMovimentacao.RECEITA,1))
+        binding.rvBankStatement.adapter = BankStatementAdapter(dataSet)
+    }
+
+
 }
